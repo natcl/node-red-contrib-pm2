de21492173a5f6b5776137305840790c7146dac8
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2017-12-13 15:47:18 -0500

    Handle exeption when payload is not a string

--- pm2/pm2.js
+++ pm2/pm2.js
@@ -11,43 +11,47 @@ module.exports = function(RED) {
                     return;
                 }
 
-                if (msg.payload === 'list') {
-                    pm2.list(function(err, processDescriptionList) {
-                        if (err) {
-                            node.error('PM2: ' + err);
-                            return;
-                        }
-                        msg.payload = processDescriptionList;
-                        node.send(msg);
-                        pm2.disconnect();
-                    });
-                }
-                if (msg.payload.startsWith('start')) {
-                    var start_proc = msg.payload.split(' ')[1];
-                    pm2.start(start_proc, function(err) {
-                        if (err) {
-                            node.error('PM2: ' + err);
-                        }
-                        pm2.disconnect();
-                    });
-                }
-                if (msg.payload.startsWith('stop')) {
-                    var stop_proc = msg.payload.split(' ')[1];
-                    pm2.stop(stop_proc, function(err) {
-                        if (err) {
-                            node.error('PM2: ' + err);
-                        }
-                        pm2.disconnect();
-                    });
-                }
-                if (msg.payload.startsWith('restart')) {
-                    var restart_proc = msg.payload.split(' ')[1];
-                    pm2.restart(restart_proc, function(err) {
-                        if (err) {
-                            node.error('PM2: ' + err);
-                        }
-                        pm2.disconnect();
-                    });
+                if (typeof msg.payload === 'string' || msg.payload instanceof String) {
+                    if (msg.payload === 'list') {
+                        pm2.list(function(err, processDescriptionList) {
+                            if (err) {
+                                node.error('PM2: ' + err);
+                                return;
+                            }
+                            msg.payload = processDescriptionList;
+                            node.send(msg);
+                            pm2.disconnect();
+                        });
+                    }
+                    if (msg.payload.startsWith('start')) {
+                        var start_proc = msg.payload.split(' ')[1];
+                        pm2.start(start_proc, function(err) {
+                            if (err) {
+                                node.error('PM2: ' + err);
+                            }
+                            pm2.disconnect();
+                        });
+                    }
+                    if (msg.payload.startsWith('stop')) {
+                        var stop_proc = msg.payload.split(' ')[1];
+                        pm2.stop(stop_proc, function(err) {
+                            if (err) {
+                                node.error('PM2: ' + err);
+                            }
+                            pm2.disconnect();
+                        });
+                    }
+                    if (msg.payload.startsWith('restart')) {
+                        var restart_proc = msg.payload.split(' ')[1];
+                        pm2.restart(restart_proc, function(err) {
+                            if (err) {
+                                node.error('PM2: ' + err);
+                            }
+                            pm2.disconnect();
+                        });
+                    }
+                } else {
+                    node.error("Payload must be a string", msg);
                 }
             });
         });
